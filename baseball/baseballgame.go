dd264870cde55b0bd8c8e6efdf015bd7f24d4355
reallucky84 <reallucky84@gmail.com>
2020-04-27 15:22:33 +0900

    - git ignore 추가 - make number 추가중

--- baseball/baseballgame.go
+++ baseball/baseballgame.go
@@ -1,12 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main(){
-	fmt.Println("Baseball Game.go")
-	
+	// Make number for play
+	gameNum := makeNumber()
+	fmt.Println("gameNumber=",gameNum)
+	// get input from player
+	inputNum := getInputNumber()
+
+	// show result
+	showResult(inputNum)
 }
 
-func input()  {
+func makeNumber() [3]int{
+	var gameNum [3]int
+	for i := 0 ; i < 3; i++{
+		gameNum[i] = rand.Intn(10) + 1
+	}
+	return gameNum
+}
 
+func getInputNumber() [3]int{
+
+	return [3]int{}
+}
+
+func showResult(inputNum [3]int)  {
+
+	fmt.Println("inputNum=", inputNum)
 }
