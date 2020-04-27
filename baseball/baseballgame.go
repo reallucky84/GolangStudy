bf61010e32b4312d9a2ea0dc6c48da5a05527eab
reallucky84 <reallucky84@gmail.com>
2020-04-27 15:35:20 +0900

    - makeNumber 숫자 중복제거 로직 추가

--- baseball/baseballgame.go
+++ baseball/baseballgame.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
-func main(){
+func main() {
 	// Make number for play
 	gameNum := makeNumber()
-	fmt.Println("gameNumber=",gameNum)
+	fmt.Println("gameNumber=", gameNum)
+
 	// get input from player
 	inputNum := getInputNumber()
 
@@ -16,20 +18,36 @@ func main(){
 	showResult(inputNum)
 }
 
-func makeNumber() [3]int{
+func makeNumber() [3]int {
 	var gameNum [3]int
-	for i := 0 ; i < 3; i++{
-		gameNum[i] = rand.Intn(10) + 1
+	for i := 0; i < 3; i++ {
+		for {
+			rand.Seed(time.Now().UnixNano())
+			tmpNum := rand.Intn(9) + 1
+			if checkIfUsableNum(tmpNum, gameNum) {
+				gameNum[i] = tmpNum
+				break
+			}
+		}
 	}
 	return gameNum
 }
 
-func getInputNumber() [3]int{
+func checkIfUsableNum(num int, gameNum [3]int) bool {
+	for i := 0; i < 3; i++ {
+		if gameNum[i] == num {
+			return false
+		}
+	}
+	return true
+}
+
+func getInputNumber() [3]int {
 
 	return [3]int{}
 }
 
-func showResult(inputNum [3]int)  {
+func showResult(inputNum [3]int) {
 
 	fmt.Println("inputNum=", inputNum)
 }
