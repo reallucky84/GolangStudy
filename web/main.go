143f70e21bef2d3c0c2f7b838b9b580510f45b8e
hyuny <notimeforlunchs@gmail.com>
2020-05-10 23:05:17 +0900

    - webserver first commit

---
+++ web/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type barHandler struct{}
+
+func (b barHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello bar1.")
+}
+
+func (b *barHandler) aaa() {
+
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello world!")
+	})
+
+	http.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This is foo.")
+	})
+
+	http.Handle("/bar", &barHandler{})
+
+	http.ListenAndServe(":3000", nil)
+}
